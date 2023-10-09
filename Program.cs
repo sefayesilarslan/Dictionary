4dc6cb6ac65f3885f1088a4ef91acf12480a0f71
Sefa <sefayesilarslan1922@gmail.com>
2023-10-09 16:55:04 +0300

    Dictionary

---
+++ Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Dictionary
+{
+    class Dictionary
+    {
+
+        public static void Main(string[] args)
+        {
+            Dictionary<int, string> kullanicilar = new Dictionary<int, string>();
+
+            kullanicilar.Add(10, "Sefa Yeşilarslan");
+            kullanicilar.Add(6, "Ahmet Çalık");
+            kullanicilar.Add(17, "Sokol Chikalleshi");
+            kullanicilar.Add(18, "Ahmet Oğuz");
+
+            Console.WriteLine("****Elemanlara Erişimm********");
+
+            Console.WriteLine(kullanicilar[17]);
+
+            foreach (var kullanici in kullanicilar)
+            {
+                Console.WriteLine(kullanici);
+            }
+
+
+            // COUNT
+            Console.WriteLine("****Count********");
+            Console.WriteLine(kullanicilar.Count);
+
+            // Contais
+            Console.WriteLine("****Contais********");
+            Console.WriteLine(kullanicilar.ContainsKey(10));
+            Console.WriteLine(kullanicilar.ContainsValue("Ahmet Çalık"));
+
+            // Remove
+            Console.WriteLine("****Remove********");
+            kullanicilar.Remove(18);
+
+            foreach (var kullanici in kullanicilar)
+            {
+                Console.WriteLine(kullanici);
+            }
+
+
+            // Key ve Value
+
+            foreach (var kullanici in kullanicilar)
+            {
+                Console.WriteLine(kullanici.Value);
+            }
+
+
+
+
+
+        }
+    }
+}
